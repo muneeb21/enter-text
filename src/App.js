76feecf763ad3d4f63e733e495f7e3acbc1356b5
Muneeb Adil <muneebadil07@gmail.com>
2020-10-25 22:35:47 +0530

    setup the project and write main app file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import ReactDOM from 'react-dom';
 
-function App() {
+class App extends React.Component {
+
+  constructor() {
+    super();
+    // defining the state at first
+    this.state = 
+    { firstBox: "",
+      secondBox:""
+    }
+  }
+
+  // handler for form data
+
+  textHandler=(e)=>{
+    e.preventDefault();
+    const text=e.target.value;
+    console.log(text," text");
+  }
+
+  keyOneHandler=(e)=>{
+    // e.preventDefault();
+    const text=e.target.value;
+    console.log(text," key1");
+  }
+
+  keyTwoHandler=(e)=>{
+    // e.preventDefault();
+    const text=e.target.value;
+    console.log(text," key2");
+  }
+
+  render(){
+    const firstBox=this.state.firstBox;
+    const secondBox=this.state.secondBox;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div classnname="form">
+        <form >
+           <input type="text" name="key1"onChange={this.keyOneHandler} placeholder="Enter Key1.."/>
+           <input type="text" name="key2"onChange={this.keyTwoHandler}placeholder="Enter Key2.." />
+           <textarea rows="4" cols="50" placeholder="Enter Text...." name="text"onChange={this.textHandler}>
+
+           </textarea>
+           
+          </form> 
+          
+      </div>
+      <div id="firstBox">
+         {firstBox}
+      </div>
+      <div id="secondBox">
+          {secondBox}
+      </div>
     </div>
   );
 }
+}
+
+// const style={
+
+// }
 
 export default App;
